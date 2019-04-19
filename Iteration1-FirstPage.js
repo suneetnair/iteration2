edf69ec5c77c9cd09a67fa188b8152049f29a3e8
Davidxxye <43427491+Davidxxye@users.noreply.github.com>
2019-04-19 22:46:56 +1000

    Delete Iteration1-FirstPage.js

--- Iteration1-FirstPage.js
+++
@@ -1,9 +0,0 @@
-// set the password of the website
-var password;
-var pass = "physis";
-password = prompt('Please enter the password to view the page', '');
-if (password == pass)
-    alert('Correct password, click ok to enter.');
-else {
-    window.location = "http://wwww.google.com.au"
-}
